4b7a84fe99acfe80026a5b9b51a60789385b11de
soundboy809 <raul2762@hotmail.com>
2018-01-25 02:27:15 -0400

    Agregar archivos de proyecto.

---
+++ Update_softwareRM/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace Update_softwareRM
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.metroProgressBar1 = new MetroFramework.Controls.MetroProgressBar();
+            this.metroButton1 = new MetroFramework.Controls.MetroButton();
+            this.status = new MetroFramework.Controls.MetroLabel();
+            this.status_speed = new MetroFramework.Controls.MetroLabel();
+            this.status_porcent = new MetroFramework.Controls.MetroLabel();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.status_count = new MetroFramework.Controls.MetroLabel();
+            this.SuspendLayout();
+            // 
+            // metroProgressBar1
+            // 
+            this.metroProgressBar1.Location = new System.Drawing.Point(102, 130);
+            this.metroProgressBar1.Name = "metroProgressBar1";
+            this.metroProgressBar1.Size = new System.Drawing.Size(269, 23);
+            this.metroProgressBar1.TabIndex = 0;
+            // 
+            // metroButton1
+            // 
+            this.metroButton1.Location = new System.Drawing.Point(203, 72);
+            this.metroButton1.Name = "metroButton1";
+            this.metroButton1.Size = new System.Drawing.Size(101, 27);
+            this.metroButton1.TabIndex = 1;
+            this.metroButton1.Text = "Descargar";
+            this.metroButton1.UseSelectable = true;
+            this.metroButton1.Click += new System.EventHandler(this.metroButton1_Click);
+            // 
+            // status
+            // 
+            this.status.AutoSize = true;
+            this.status.FontSize = MetroFramework.MetroLabelSize.Tall;
+            this.status.FontWeight = MetroFramework.MetroLabelWeight.Bold;
+            this.status.Location = new System.Drawing.Point(102, 102);
+            this.status.Name = "status";
+            this.status.Size = new System.Drawing.Size(38, 25);
+            this.status.TabIndex = 2;
+            this.status.Text = "0%";
+            // 
+            // status_speed
+            // 
+            this.status_speed.AutoSize = true;
+            this.status_speed.FontSize = MetroFramework.MetroLabelSize.Tall;
+            this.status_speed.FontWeight = MetroFramework.MetroLabelWeight.Bold;
+            this.status_speed.Location = new System.Drawing.Point(102, 156);
+            this.status_speed.Name = "status_speed";
+            this.status_speed.Size = new System.Drawing.Size(22, 25);
+            this.status_speed.TabIndex = 3;
+            this.status_speed.Text = "0";
+            this.status_speed.Visible = false;
+            // 
+            // status_porcent
+            // 
+            this.status_porcent.AutoSize = true;
+            this.status_porcent.FontSize = MetroFramework.MetroLabelSize.Tall;
+            this.status_porcent.FontWeight = MetroFramework.MetroLabelWeight.Bold;
+            this.status_porcent.Location = new System.Drawing.Point(377, 130);
+            this.status_porcent.Name = "status_porcent";
+            this.status_porcent.Size = new System.Drawing.Size(38, 25);
+            this.status_porcent.TabIndex = 4;
+            this.status_porcent.Text = "0%";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // status_count
+            // 
+            this.status_count.AutoSize = true;
+            this.status_count.FontSize = MetroFramework.MetroLabelSize.Tall;
+            this.status_count.FontWeight = MetroFramework.MetroLabelWeight.Bold;
+            this.status_count.Location = new System.Drawing.Point(23, 190);
+            this.status_count.Name = "status_count";
+            this.status_count.Size = new System.Drawing.Size(17, 25);
+            this.status_count.TabIndex = 5;
+            this.status_count.Text = ".";
+            this.status_count.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(463, 221);
+            this.Controls.Add(this.status_count);
+            this.Controls.Add(this.status_porcent);
+            this.Controls.Add(this.status_speed);
+            this.Controls.Add(this.status);
+            this.Controls.Add(this.metroButton1);
+            this.Controls.Add(this.metroProgressBar1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Resizable = false;
+            this.Text = "Actualización de Software";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private MetroFramework.Controls.MetroProgressBar metroProgressBar1;
+        private MetroFramework.Controls.MetroButton metroButton1;
+        private MetroFramework.Controls.MetroLabel status;
+        private MetroFramework.Controls.MetroLabel status_speed;
+        private MetroFramework.Controls.MetroLabel status_porcent;
+        private System.Windows.Forms.Timer timer1;
+        private MetroFramework.Controls.MetroLabel status_count;
+    }
+}
+
---
+++ Update_softwareRM/Form1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MetroFramework.Forms;
+using System.Net;
+using System.Diagnostics;
+using Ionic.Zip;
+
+namespace Update_softwareRM
+{
+    public partial class Form1 : MetroForm
+    {
+        Stopwatch sw = new Stopwatch();
+        int count = 10;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public void dfj(bool visibilidad)
+        {
+            Visible = visibilidad;
+            status.Text = "Iniciando descarga...";
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        // Descomprimir Zip
+        static public Boolean DescromprimirZip(string ArchivoZip, string RutaGuardar)
+        {
+            try
+            {
+                using (ZipFile zip = ZipFile.Read(ArchivoZip))
+                {
+
+                    zip.ExtractAll(RutaGuardar);
+                    zip.Dispose();
+
+                }
+
+                return true;
+            }
+            catch
+            {
+                return false;
+
+            }
+
+        }
+        // Comprobar conexion
+        public static bool CheckInternetConnection()
+        {
+            try
+            {
+                using (var client = new WebClient())
+                {
+                    using (var stream = client.OpenRead("http://www.google.com.do"))
+                    {
+                        return true;
+                    }
+
+                }
+            }
+            catch
+            {
+
+                return false;
+            }
+        }
+        private void metroButton1_Click(object sender, EventArgs e)
+        {
+            if (CheckInternetConnection())
+            {
+                dfj(true);
+                WebClient webClient = new WebClient();
+                webClient.DownloadFileCompleted += new AsyncCompletedEventHandler(Completed);
+                webClient.DownloadProgressChanged += new DownloadProgressChangedEventHandler(ProgressChanged);
+                webClient.DownloadFileAsync(new Uri("https://occd-ocoainc.org/update_program/Update.zip"), "Update_OCCD.zip");
+                sw.Start();
+            }
+            else
+            {
+                MessageBox.Show("No hay conexión a internet", "Error en descarga", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            
+        }
+
+        private void ProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            metroProgressBar1.Value = e.ProgressPercentage;
+            status.Text = "Descargando actualizacion... ";
+            status_porcent.Text = e.ProgressPercentage + "%";
+            status_speed.Text = string.Format("{0} kb/s", (e.BytesReceived / 1024d / sw.Elapsed.TotalSeconds).ToString("0.00"));
+
+        }
+
+        private void Completed(object sender, AsyncCompletedEventArgs e)
+        {
+            status_speed.Visible = false;
+            status.Text = "Descarga Completada!";
+            status.Text = "Descomprimiendo...";
+            string ruta1 = @"./Update_OCCD.zip";
+            string ruta2 = Environment.CurrentDirectory;
+            DescromprimirZip(ruta1, ruta2);
+            status.Text = "Actualizacion Instalada!";
+            status_count.Visible = true;
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            status_count.Text = $"Iniciando Programa... {count.ToString()}s";
+            count--;
+            if (count == 0)
+            {
+                timer1.Stop();
+                Application.Exit();
+            }
+        }
+    }
+}
